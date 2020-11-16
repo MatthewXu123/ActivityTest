f5618cebb3c20ec2098ed74b63edb11d6e6458fb
Matthew Xu <matthew.xu@carel.locale>
2020-11-16 12:24:28 +0800

    1116

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,11 +1,13 @@
 package com.example.activitytest;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -30,6 +32,7 @@ public class FirstActivity extends AppCompatActivity {
         });
 */
         // 显式Intent
+/*
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +41,7 @@ public class FirstActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+*/
         //隐式Intent
 /*
         btn.setOnClickListener(new View.OnClickListener() {
@@ -69,8 +73,27 @@ public class FirstActivity extends AppCompatActivity {
             }
         });
 */
+        // startActivityForResult
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 
     @Override
--- app/src/main/java/com/example/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,8 +14,27 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
-        Intent intent = getIntent();
-        String data = intent.getStringExtra("data");
-        Log.d("SecondActivity",data);
+        /*Intent intent = getIntent();
+        String data = intent.getStringExtra("data");*/
+
+        Button btn2 = (Button)findViewById(R.id.btn_2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data","data");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data","back pressed");
+        intent.putExtra("data2", "try it");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
